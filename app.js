032951347cdcd498f9ef093dc0acb4e8d41e074d
Maggy Prajapati <mupraj10@gmail.com>
2017-06-13 10:48:26 -0400

    configured express

--- app.js
+++ app.js
@@ -6,15 +6,23 @@ app.listen(3000, function(){
     console.log("server listening");
 });
 
+
+app.use(function (req, res, next) {
+    console.log(req.method + " " + req.originalUrl);
+    next();
+    // do your logging here
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+})
+
+
 app.get("/", function(request, response){
     response.send("Welcome!")
 });
 
 app.get("/news", function(request, response){
     response.send("This is the news!!")
-})
+});
 
-app.use(function (req, res, next) {
-    // do your logging here
-    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-})
+app.post("/", function(request,response){
+    response.send("Posting is working! \n");
+});
